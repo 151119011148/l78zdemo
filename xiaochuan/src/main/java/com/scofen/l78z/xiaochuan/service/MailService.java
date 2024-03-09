af4d4139bf18395b4ab235bf9936668c61504abd
gaofeng <352415401@qq.com>
2024-03-09 11:12:50 +0800

    wheel项目 测通：用户，类目，上传文件，商品，购物车 待测：邮件

--- xiaochuan/src/main/java/com/scofen/l78z/xiaochuan/service/MailService.java
+++ xiaochuan/src/main/java/com/scofen/l78z/xiaochuan/service/MailService.java
@@ -1,21 +1,14 @@
 package com.scofen.l78z.xiaochuan.service;
 
-import com.alibaba.fastjson.JSON;
-import com.scofen.l78z.xiaochuan.common.exception.ResultCode;
-import com.scofen.l78z.xiaochuan.common.exception.ServiceException;
-import com.scofen.l78z.xiaochuan.controller.request.BasketParam;
+import com.scofen.l78z.xiaochuan.controller.request.MailParam;
 import com.scofen.l78z.xiaochuan.dao.MailDao;
-import com.scofen.l78z.xiaochuan.dao.dataObject.BasketDO;
 import com.scofen.l78z.xiaochuan.dao.dataObject.MailDO;
 import org.dozer.Mapper;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 
 @Service
 public class MailService {
@@ -26,42 +19,26 @@ public class MailService {
     @Resource
     Mapper beanMapper;
 
-    public MailDO add(BasketParam param) {
-        MailDO record = this.get(param.getVisitId());
-        if (Objects.isNull(record)) {
-            record = beanMapper.map(param, MailDO.class);
-            record.setProductIds(JSON.toJSONString(param.getProductIds()));
-            record.setIsRemoved(0);
-            mailDao.save(record);
-        } else {
-            Set<String> productIds = new HashSet<>((List<String>) JSON.parseObject(record.getProductIds(), List.class));
-            productIds.addAll(param.getProductIds());
-            record.setProductIds(JSON.toJSONString(productIds));
-            mailDao.save(record);
-        }
+    public void send(MailParam param) {
+        this.add(param);
+        // TODO: 2024/3/9 发送邮件
+    }
+
+    public MailDO add(MailParam param) {
+        MailDO record = beanMapper.map(param, MailDO.class);
+        record.setIsRemoved(0);
+        mailDao.save(record);
         return record;
     }
 
 
-    public MailDO remove(BasketParam param) {
-        MailDO record = this.get(param.getVisitId());
-        if (Objects.isNull(record)) {
-            throw new ServiceException(ResultCode.DATA_NOT_EXIST.getCode(), "current basket is invalid！");
-        } else {
-            Set<String> productIds = new HashSet<>((List<String>) JSON.parse(record.getProductIds()));
-            productIds.removeAll(param.getProductIds());
-            record.setProductIds(JSON.toJSONString(productIds));
-            mailDao.save(record);
-        }
-        return record;
-    }
-
-    public MailDO get(String email) {
+    public List<MailDO> findAllByEmail(String email) {
         MailDO query = new MailDO();
         query.setIsRemoved(0);
         Example<MailDO> example = Example.of(query);
-        return mailDao.findOne(example).orElse(null);
+        return mailDao.findAll(example);
     }
+
 
 
 }
